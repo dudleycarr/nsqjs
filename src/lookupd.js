a8e0ffe36a0a8d74eb3fb29a5f7b8519ff01a35b
Benjamin Coe <ben@npmjs.com>
2017-05-04 18:46:33 -0700

    fix: bug with request not populating data object in transpiled code

--- src/lookupd.js
+++ src/lookupd.js
@@ -24,13 +24,18 @@ function lookupdRequest(url, callback) {
     timeout: 2000,
   };
 
-  request(options, (err, response, data = {}) => {
-    if (err || data.status_code !== 200) {
+  request(options, (err, response) => {
+    if (err) {
       return callback(err, []);
     }
 
+    const data = response.body
+    const statusCode = (data ? data.status_code : null) || response.statusCode
+    if (statusCode !== 200) {
+      return callback(null, []);
+    }
+
     try {
-      const { status_code: statusCode } = response;
       let { producers } = data;
 
       // Support pre version 1.x lookupd response.
