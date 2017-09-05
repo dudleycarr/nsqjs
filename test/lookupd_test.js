d9d4c847d25de14cd5daee1e2af2f61e87c8092b
Dudley Carr <dudley.carr@gmail.com>
2017-09-04 21:06:40 -0700

    Convert to standard JS style (#175)

--- test/lookupd_test.js
+++ test/lookupd_test.js
@@ -12,8 +12,8 @@ const NSQD_1 = {
   remote_address: 'localhost:12345',
   tcp_port: 4150,
   topics: ['sample_topic'],
-  version: '0.2.23',
-};
+  version: '0.2.23'
+}
 const NSQD_2 = {
   address: 'localhost',
   broadcast_address: 'localhost',
@@ -22,8 +22,8 @@ const NSQD_2 = {
   remote_address: 'localhost:56789',
   tcp_port: 5150,
   topics: ['sample_topic'],
-  version: '0.2.23',
-};
+  version: '0.2.23'
+}
 const NSQD_3 = {
   address: 'localhost',
   broadcast_address: 'localhost',
@@ -32,8 +32,8 @@ const NSQD_3 = {
   remote_address: 'localhost:23456',
   tcp_port: 6150,
   topics: ['sample_topic'],
-  version: '0.2.23',
-};
+  version: '0.2.23'
+}
 const NSQD_4 = {
   address: 'localhost',
   broadcast_address: 'localhost',
@@ -42,24 +42,24 @@ const NSQD_4 = {
   remote_address: 'localhost:34567',
   tcp_port: 7150,
   topics: ['sample_topic'],
-  version: '0.2.23',
-};
+  version: '0.2.23'
+}
 
-const LOOKUPD_1 = '127.0.0.1:4161';
-const LOOKUPD_2 = '127.0.0.1:5161';
-const LOOKUPD_3 = 'http://127.0.0.1:6161/';
-const LOOKUPD_4 = 'http://127.0.0.1:7161/path/lookup';
+const LOOKUPD_1 = '127.0.0.1:4161'
+const LOOKUPD_2 = '127.0.0.1:5161'
+const LOOKUPD_3 = 'http://127.0.0.1:6161/'
+const LOOKUPD_4 = 'http://127.0.0.1:7161/path/lookup'
 
 const nockUrlSplit = url => {
-  const match = url.match(/^(https?:\/\/[^/]+)(\/.*$)/i);
+  const match = url.match(/^(https?:\/\/[^/]+)(\/.*$)/i)
   return {
     baseUrl: match[1],
-    path: match[2],
-  };
-};
+    path: match[2]
+  }
+}
 
 const registerWithLookupd = (lookupdAddress, nsqd) => {
-  const producers = nsqd != null ? [nsqd] : [];
+  const producers = nsqd != null ? [nsqd] : []
 
   if (nsqd != null) {
     nsqd.topics.forEach(topic => {
@@ -69,104 +69,110 @@ const registerWithLookupd = (lookupdAddress, nsqd) => {
           .reply(200, {
             status_code: 200,
             status_txt: 'OK',
-            producers,
-          });
+            producers
+          })
       } else {
-        const params = nockUrlSplit(lookupdAddress);
-        const { baseUrl } = params;
-        let { path } = params;
+        const params = nockUrlSplit(lookupdAddress)
+        const { baseUrl } = params
+        let { path } = params
         if (!path || path === '/') {
-          path = '/lookup';
+          path = '/lookup'
         }
 
-        nock(baseUrl).get(`${path}?topic=${topic}`).reply(200, {
-          status_code: 200,
-          status_txt: 'OK',
-          producers,
-        });
+        nock(baseUrl)
+          .get(`${path}?topic=${topic}`)
+          .reply(200, {
+            status_code: 200,
+            status_txt: 'OK',
+            producers
+          })
       }
-    });
+    })
   }
-};
+}
 
 const setFailedTopicReply = (lookupdAddress, topic) =>
-  nock(`http://${lookupdAddress}`).get(`/lookup?topic=${topic}`).reply(200, {
-    status_code: 404,
-    status_txt: 'TOPIC_NOT_FOUND',
-  });
+  nock(`http://${lookupdAddress}`)
+    .get(`/lookup?topic=${topic}`)
+    .reply(200, {
+      status_code: 404,
+      status_txt: 'TOPIC_NOT_FOUND'
+    })
 
 describe('lookupd.lookup', () => {
-  afterEach(() => nock.cleanAll());
+  afterEach(() => nock.cleanAll())
 
   describe('querying a single lookupd for a topic', () => {
     it('should return an empty list if no nsqd nodes', done => {
-      setFailedTopicReply(LOOKUPD_1, 'sample_topic');
+      setFailedTopicReply(LOOKUPD_1, 'sample_topic')
 
       lookup(LOOKUPD_1, 'sample_topic', (err, nodes) => {
-        nodes.should.be.empty();
-        done(err);
-      });
-    });
+        nodes.should.be.empty()
+        done(err)
+      })
+    })
 
     it('should return a list of nsqd nodes for a success reply', done => {
-      registerWithLookupd(LOOKUPD_1, NSQD_1);
+      registerWithLookupd(LOOKUPD_1, NSQD_1)
 
       lookup(LOOKUPD_1, 'sample_topic', (err, nodes) => {
-        nodes.should.have.length(1);
-        [
+        nodes.should.have.length(1)
+        ;[
           'address',
           'broadcast_address',
           'tcp_port',
-          'http_port',
+          'http_port'
         ].forEach(key => {
-          should.ok(_.keys(nodes[0]).includes(key));
-        });
-        done(err);
-      });
-    });
-  });
+          should.ok(_.keys(nodes[0]).includes(key))
+        })
+        done(err)
+      })
+    })
+  })
 
   describe('querying a multiple lookupd', () => {
     it('should combine results from multiple lookupds', done => {
-      registerWithLookupd(LOOKUPD_1, NSQD_1);
-      registerWithLookupd(LOOKUPD_2, NSQD_2);
-      registerWithLookupd(LOOKUPD_3, NSQD_3);
-      registerWithLookupd(LOOKUPD_4, NSQD_4);
+      registerWithLookupd(LOOKUPD_1, NSQD_1)
+      registerWithLookupd(LOOKUPD_2, NSQD_2)
+      registerWithLookupd(LOOKUPD_3, NSQD_3)
+      registerWithLookupd(LOOKUPD_4, NSQD_4)
 
-      const lookupdAddresses = [LOOKUPD_1, LOOKUPD_2, LOOKUPD_3, LOOKUPD_4];
+      const lookupdAddresses = [LOOKUPD_1, LOOKUPD_2, LOOKUPD_3, LOOKUPD_4]
       lookup(lookupdAddresses, 'sample_topic', (err, nodes) => {
-        nodes.should.have.length(4);
+        nodes.should.have.length(4)
         _.chain(nodes)
           .map(n => n['tcp_port'])
           .sort()
           .value()
-          .should.be.eql([4150, 5150, 6150, 7150]);
-        done(err);
-      });
-    });
+          .should.be.eql([4150, 5150, 6150, 7150])
+        done(err)
+      })
+    })
 
     it('should dedupe combined results', done => {
-      registerWithLookupd(LOOKUPD_1, NSQD_1);
-      registerWithLookupd(LOOKUPD_2, NSQD_1);
-      registerWithLookupd(LOOKUPD_3, NSQD_1);
-      registerWithLookupd(LOOKUPD_4, NSQD_1);
+      registerWithLookupd(LOOKUPD_1, NSQD_1)
+      registerWithLookupd(LOOKUPD_2, NSQD_1)
+      registerWithLookupd(LOOKUPD_3, NSQD_1)
+      registerWithLookupd(LOOKUPD_4, NSQD_1)
 
-      const lookupdAddresses = [LOOKUPD_1, LOOKUPD_2, LOOKUPD_3, LOOKUPD_4];
+      const lookupdAddresses = [LOOKUPD_1, LOOKUPD_2, LOOKUPD_3, LOOKUPD_4]
       lookup(lookupdAddresses, 'sample_topic', (err, nodes) => {
-        nodes.should.have.length(1);
-        done(err);
-      });
-    });
+        nodes.should.have.length(1)
+        done(err)
+      })
+    })
 
     return it('should succeed inspite of failures to query a lookupd', done => {
-      registerWithLookupd(LOOKUPD_1, NSQD_1);
-      nock(`http://${LOOKUPD_2}`).get('/lookup?topic=sample_topic').reply(500);
+      registerWithLookupd(LOOKUPD_1, NSQD_1)
+      nock(`http://${LOOKUPD_2}`)
+        .get('/lookup?topic=sample_topic')
+        .reply(500)
 
-      const lookupdAddresses = [LOOKUPD_1, LOOKUPD_2];
+      const lookupdAddresses = [LOOKUPD_1, LOOKUPD_2]
       lookup(lookupdAddresses, 'sample_topic', (err, nodes) => {
-        nodes.should.have.length(1);
-        done(err);
-      });
-    });
-  });
-});
+        nodes.should.have.length(1)
+        done(err)
+      })
+    })
+  })
+})
