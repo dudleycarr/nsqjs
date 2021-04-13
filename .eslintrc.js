c3b424aaa7f9df8fd28257824564537ae635dbff
Dudley Carr <dudley.carr@gmail.com>
2021-04-13 09:24:52 -0700

    feat: remove BigNumber.js and replace Int64 with BigInt

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   env: {
+    es2020: true,
     node: true,
   },
   extends: ['eslint:recommended'],
--- test/rawmessage.js
+++ test/rawmessage.js
@@ -1,13 +1,10 @@
-const Int64 = require('node-int64')
-
 function rawMessage(id, timestamp, attempts, body) {
   // Create the raw NSQ message
   id = Buffer.from(id)
-  timestamp = new Int64(timestamp).toBuffer()
   body = Buffer.from(body)
 
   const b = Buffer.alloc(8 + 2 + 16 + body.length)
-  b.copy(timestamp, 0, 0, 8)
+  b.writeBigInt64BE(BigInt(timestamp), 0)
   b.writeInt16BE(attempts, 8)
   b.copy(id, 10, 0, 16)
   b.copy(body, 26, 0, body.length)
